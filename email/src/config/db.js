2a1bd0e8abc98aec082584b5fbca18b823edf2b2
Viraj Gupta <vgupta2@q3infotech.net>
2023-09-03 23:40:23 +0530

    fix: hide secret sources and remove node modules

--- email/src/config/db.js
+++ email/src/config/db.js
@@ -1,17 +1,5 @@
 const mongoose = require("mongoose");
 
-
 module.exports= ()=>{
-    return mongoose.connect("mongodb+srv://virajgupta:Virajgupta12345@cluster0.wmplk.mongodb.net/email?retryWrites=true&w=majority")
+    return mongoose.connect(`mongodb+srv://${env.SECRET_KEY}@cluster0.wmplk.mongodb.net/email?retryWrites=true&w=majority`)
 };
-
-
-//OR
-
-// const mongoose = require("mongoose");
-
-// const connect = () =>{
-//     return mongoose.connect("mongodb+srv://virajgupta:Virajgupta12345@cluster0.wmplk.mongodb.net/mvc?retryWrites=true&w=majority")
-// }
-
-// module.exports = connect;
--- library/index.js
+++ library/index.js
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json());
 
 const connect = () => {
-  return mongoose.connect(
-    "mongodb+srv://virajgupta:Virajgupta12345@cluster0.wmplk.mongodb.net/library?retryWrites=true&w=majority"
-  );
+  return mongoose.connect(`mongodb+srv://${env.SECRET_KEY}@cluster0.wmplk.mongodb.net/email?retryWrites=true&w=majority`)
 };
 
 // Section Schema
